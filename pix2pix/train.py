b83e79497f752cd5078b7d67c265ef4dc0d3b5ff
lmg1404 <luismg0203@gmail.com>
2023-12-06 15:04:22 -0500

    Basic hyperparameters and device

--- pix2pix/train.py
+++ pix2pix/train.py
@@ -10,3 +10,17 @@ from torch.utils.tensorboard import SummaryWriter
 from models import Discriminator, Generator
 from tqdm import tqdm
 
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# hyperparameters
+LEARNING_RATE = 2e-4            # same as dc gan
+BATCH_SIZE = 1                  # 1-10 depending on what we're doing
+IMAGE_SIZE = 256
+CHANNEL_IMG = 3                 # check this
+EPOCHS = 50                     # anywhere from 50-75 would be good I think, early stopping is an option through board
+BETA_1 = 0.5
+BETA_2 = 0.999
+LAMBDA = 100
+FEATURES_DISC = 64
+FEATURES_GEN = 64
