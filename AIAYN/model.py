f6bd8ba0c6e2fed2423be056386174e3b14a97ad
lmg1404 <luismg0203@gmail.com>
2024-02-28 14:03:01 -0500

    Transformer remaining

--- AIAYN/model.py
+++ AIAYN/model.py
@@ -19,20 +19,18 @@ class Head(nn.Module):
         self.key = nn.Linear(in_features=embed_dim, out_features=head_dim, bias=False)
         self.query = nn.Linear(in_features=embed_dim, out_features=head_dim, bias=False)
         self.value= nn.Linear(in_features=embed_dim, out_features=head_dim, bias=False)
-        
-        # TODO: figure out register buffer
-        # TODO: prototype tril
+
         self.register_buffer('mask', torch.tril(torch.ones(head_dim, head_dim), diagonal=1))
         self.mask_bool = mask
         
         self.dropout = nn.Dropout(D_PROB)
     
-    def forward(self, x):
+    def forward(self, key, query, value):
         # t for tokens, so I think this is words in this case
-        _, t, d_k = x.size() 
-        k = self.key(x)
-        q = self.query(x)
-        v = self.value(x)
+        _, t, d_k = key.size() 
+        k = self.key(key)
+        q = self.query(query)
+        v = self.value(value)
         qk = (q@k.transpose(-1, -2)) / d_k ** 0.5
         
         if self.mask_bool:
@@ -57,56 +55,61 @@ class MultiHeadAttention(nn.Module):
         # TODO: figure out dims
         self.linear = nn.Linear(in_features=embed_dim, out_features=embed_dim)
     
-    def forward(self, x):
+    def forward(self, key, query, value):
         # we know dim -1 will be dim_model // num_heads so cat returns to dim_model
-        cat = torch.cat([head(x) for head in self.multihead], dim=-1)
+        cat = torch.cat([head(key, query, value) for head in self.multihead], dim=-1)
         out = self.linear(cat)
         return out
 
 class FeedForward(nn.Module):
     def __init__(self, embed_dim: int):
+        super().__init__()
         # see section 3.3
         self.sublayer = nn.Sequential(
             nn.Linear(in_features=embed_dim, out_features=4 * embed_dim),
             nn.ReLU(),
-            nn.Linear(in_features=embed_dim, out_features=4* embed_dim)
+            nn.Linear(in_features=4 * embed_dim, out_features=embed_dim)
         )
     
     def forward(self, x):
         return self.sublayer(x)
 
-# TODO
 class EncoderBlock(nn.Module):
-    def __init__(self, num_heads: int, head_dim: int):
-         self.mh = MultiHeadAttention(num_heads, head_dim)
-         self.ln1 = nn.LayerNorm()
-         self.ff = FeedForward() # TODO: change once prototyped
-         self.ln2 = nn.LayerNorm()
+    def __init__(self, num_heads: int, embed_dim: int):
+        super().__init__()
+        head_dim = embed_dim // num_heads
+        self.mh = MultiHeadAttention(num_heads, head_dim, embed_dim, False)
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.ff = FeedForward(embed_dim) 
+        self.ln2 = nn.LayerNorm(embed_dim)
     
     def forward(self, x):
-        x = self.ln1(x + self.multihead(x))
+        x = self.ln1(x + self.mh(x, x, x))
         x = self.ln2(x + self.ff(x))
         return x
 
-# TODO
 class DecoderBlock(nn.Module):
-    def __init__(self, num_heads: int, head_dim: int):
-        self.mh1 = MultiHeadAttention(num_heads, head_dim)
-        self.ln1 = nn.LayerNorm()
-        self.mh2 = MultiHeadAttention(num_heads, head_dim)
-        self.ln2 = nn.LayerNorm()
-        self.ff  = FeedForward() # TODO: fix
-        self.ln3 = nn.LayerNorm()
+    def __init__(self, num_heads: int, embed_dim: int):
+        super().__init__()
+        head_dim = embed_dim // num_heads
+        self.mh1 = MultiHeadAttention(num_heads, head_dim, embed_dim, True)
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.mh2 = MultiHeadAttention(num_heads, head_dim, embed_dim, False)
+        self.ln2 = nn.LayerNorm(embed_dim)
+        self.ff  = FeedForward(embed_dim) # TODO: fix
+        self.ln3 = nn.LayerNorm(embed_dim)
     
     def forward(self, x, enc: torch.Tensor):
-        x = self.ln1(x + self.mh1(x))
-        x = self.ln2(x + self.mh2(x + enc)) # TODO: fix
+        assert x.size() == enc.size(), f"Encoder output and decoder sublayer 1 output must be same shape: {enc.size()} {x.size()}"
+        x = self.ln1(x + self.mh1(x, x, x))
+        x = self.ln2(x + self.mh2(enc, x, enc)) # TODO: fix
         x = self.ln3(x + self.ff(x))
         return x
 
 # TODO
 class Transformer(nn.Module):
     def __init__(self, N: int, num_heads: int, head_dim: int):
+        super().__init__()
         self.in_emb = nn.Embedding()
         self.out_emb = nn.Embedding()
         self.encoder = nn.Sequential(*[EncoderBlock(num_heads, head_dim) for _ in range(N)])
