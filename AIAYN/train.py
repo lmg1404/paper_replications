38056c4e98b2db2860aee23ee2d809a861238f47
lmg1404 <luismg0203@gmail.com>
2024-03-18 20:44:57 -0400

    Preparing to train pt 1

--- AIAYN/train.py
+++ AIAYN/train.py
@@ -1,16 +1,41 @@
 # import dependencies
 import torch
 import torch.nn as nn
+from torch.nn import functional
 import torch.optim as optim
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
-from model import Transformer
+from model import Transformer, Paraphrase, custom_collate_fn
 from tqdm import tqdm
+import spacy
+import time
+
+# set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # hyper parameters
 BETA_1 = 0.9
 BETA_2 = 0.98
 EPISILON = 1e-9
-EPOCHS = 20
+EPOCHS = 10
 WARMUP_STEPS = 4000
+LAYERS = 6
+HEADS = 8
+EMBED_DIM = 512
+
+spacy_en = spacy.load('en_core_web_sm')
+src_file_path = "../../data/paraphrases/train/train.src"
+tgt_file_path = "../../data/paraphrases/train/train.tgt"
+
+paraphrase_data = Paraphrase(src_file_path, tgt_file_path, spacy_en)
+VOCAB_SIZE = paraphrase_data.vocab_size()
+CONTEXT = paraphrase_data.max_context()
+loader = DataLoader(paraphrase_data, batch_size=64, collate_fn=custom_collate_fn, shuffle=True)
+
+optimus = Transformer(LAYERS, HEADS, EMBED_DIM, VOCAB_SIZE, CONTEXT).to(device)
+optimus.train()
+
+optimizer = optim.Adam(optimus.parameters(), lr=LR, betas=(BETA_1, BETA_2), eps=EPISILON)
+
+# we will have to F.crossentropy to get the loss, we can still do loss.backwards() check the docs
